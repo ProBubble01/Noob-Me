f60521594817dfa59c5f0802701c8f857d173e50
ProBubble <sreejithsree589@gmail.com>
2019-06-03 13:35:47 +0530

    calc
    my first calculator

---
+++ hello.py
@@ -0,0 +1,13 @@
+num1 = input ("first num here: ")
+num2 = input ("second number here: ")
+operation = input ("what you wanna do: ")
+
+if operation == "+":
+    result = int(num1) + int(num2)
+elif operation == "-":
+    result = int(num1) - int(num2)
+elif operation == "*":
+    result = int(num1) * int(num2)
+elif operation == "/":
+    result = int(num1) / int(num2)
+print (result)
