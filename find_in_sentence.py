5279b671cc2977a11e6a84be493622c897ba83c9
PROBUBBLE01 <46400271+PROBUBBLE01@users.noreply.github.com>
2021-01-16 12:42:10 +0530

    Update find_in_sentence.py

--- find_in_sentence.py
+++ find_in_sentence.py
@@ -1,8 +1,12 @@
 #recieves input from user
 sentence = input('Sentence: ')
 find = input('Find: ')
+
+
 #checks if find value exists in the sentence
 is_true = sentence.find(find)
+
+
 #prints output depending on is_true value
 if is_true == (-1):
     print ((find) , 'does not exist')
