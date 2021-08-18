864af6ff45f727bb43432db5edb4812a9de90ec4
Sreejith <46400271+PROBUBBLE01@users.noreply.github.com>
2021-08-18 13:30:39 +0530

    Update The_mighty_if.py

--- Mighty IF/The_mighty_if.py
+++ Mighty IF/The_mighty_if.py
@@ -2,4 +2,4 @@ fif = open("The_mighty_IF.py", "w")
 fif.write('''k = int(input("enter your number:" ))''')
 for i in range(100000000):
     fif.write(f'''\nif k == {i}:\n    if k%2 == 0:\n        print("number is even")\n    else:\n        print("number is odd")''')
-fif.close
+fif.close()
